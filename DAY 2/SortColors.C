7551c7640ea6bc845f020a4bd0c0498da2ed7fb4
mani-357 <114056647+mani-357@users.noreply.github.com>
2022-12-21 20:24:50 +0530

    Create SortColors.C

---
+++ DAY 2/SortColors.C
@@ -0,0 +1,29 @@
+void sortColors(int* nums, int n){
+    int cz=0,co=0;
+    for(int i=0;i<n;i++)
+    {
+        if(nums[i]==0)
+        {
+            cz++;
+}
+        if(nums[i]==1)
+        {
+co++;
+        }
+}
+
+    
+    for(int i=0;i<cz;i++)
+    {
+       nums[i]=0;
+    }
+    for(int i=cz;i<cz+co;i++)
+    {
+nums[i]=1;
+    }
+    for(int i=cz+co;i<n;i++)
+    {
+      nums[i]=2;
+    }
+    
+}
